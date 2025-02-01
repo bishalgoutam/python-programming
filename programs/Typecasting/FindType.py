53c0c407a307d2382e846274c2e6865d394a820a
bishal <bishalgoutam3@gmail.com>
2025-02-01 15:51:26 -0500

    Updated Typecasting examples - Added program to Find Types

---
+++ programs/Typecasting/FindType.py
@@ -0,0 +1,31 @@
+# Developed by: Bishal Goutam
+# Date: 2/1/2025
+
+# Program starts here
+
+# type(): returns the type of the data
+
+# Find data types
+print("Find data type")
+print("\n")
+print("7.8909->",type(7.8909))  #float
+print("5.7->", type(5.7))
+print("0.0->",type(0.0))
+
+print("78909->",type("78909"))  # String
+print("7.8909->",type("7.8909"))
+print("ABCD->", type("ABCD"))
+print("_->", type("_"))
+print("Space->", type(" "))
+print("Empty/Null String->", type(""))
+
+print("0->", type(0))   #Integer
+print("10->", type(10))
+print("-345->", type(-345))
+
+print("True->", type(True))  #bool
+print("True->", type(False))
+print("\n")
+
+
+# Program ends here
