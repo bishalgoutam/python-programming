5261feb534ba539183a1fed6f1317f84d6161d70
bishal <bishalgoutam3@gmail.com>
2025-02-01 15:26:53 -0500

    Updated Typecasting examples

---
+++ programs/Typecasting/TypecastingToFloat.py
@@ -0,0 +1,31 @@
+# Developed by: Bishal Goutam
+# Date: 2/1/2025
+
+# Program starts here
+
+# int(): converts to integer
+# str(): converts to string
+# float(): converts to float
+# bool(): converts to boolean
+
+# Integer to other data types
+print("Typecasting to Float")
+print("\n")
+print("Typecasting Integer to Float")
+print("7->",float(7))  # No rounding up to 2
+print("5->", float(5))
+print("\n")
+
+print("Typecasting String to Float")
+print("78909->",float("78909"))  # Within quotes only digits then it can be converted
+print("7.8909->",float("7.8909"))  # Within quotes only digits then it can be converted
+#print("ABCD->", float("ABCD"))   # Cannot be converted
+print("\n")
+
+print("Typecasting Boolean to Float")
+print("Boolean True->",float(True))
+print("Boolean False->",float(False))
+print("\n")
+
+
+# Program ends here
