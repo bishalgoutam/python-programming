cd4e4cb71acacc04cfaade2a6f3b92b9cd81ef7e
bishal <bishalgoutam3@gmail.com>
2025-02-01 14:51:14 -0500

    Updated Typecasting examples

--- programs/Typecasting/TypeCasting.py
+++ programs/Typecasting/TypeCasting.py
@@ -10,6 +10,14 @@
 
 # Integer to other data types
 print("Typecasting to Integer")
+print("Typecasting float to Integer")
+print("7.8909->",int(7.8909))  # No rounding off 2
+print("5.7->", int(5.7))
+print("1.0->",int(1.0))
+print("1.1->",int(1.1))
+print("1.4->",int(1.4))
+print("1.5->", int(1.5))  # No rounding off to 2
+print("1.8->", int(1.8))  # No rounding off to 2
 print("\n")
 
 
