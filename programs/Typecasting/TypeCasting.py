b9c6b49b38959aa3c74f9b6b47657f6fb1914556
bishal <bishalg@bu.edu>
2025-02-01 14:34:14 -0500

    Added Typecasting examples

---
+++ programs/Typecasting/TypeCasting.py
@@ -0,0 +1,15 @@
+# Developed by: Bishal Goutam
+# Date: 2/1/2025
+
+# Program starts here
+
+# int(): converts to integer
+# str(): converts to string
+# float(): converts to float
+# bool(): converts to boolean
+
+# Integer to other data types
+print("Typecasting to Integer")
+
+
+# Program ends here
