4b319f851506644fc0bb6d8a9a8102fa04d0ae1a
bishal <bishalgoutam3@gmail.com>
2025-02-01 16:02:26 -0500

    Updated Typecasting examples - Added program to Find Types

---
+++ programs/StringOperations/StringOperations.py
@@ -0,0 +1,16 @@
+# Developed by: Bishal Goutam
+# Date: 2/1/2025
+
+# Program starts here
+# print, concatenate, replication,
+# print string
+print("A")
+print('A')
+print("Hello Python! This is one line string.")
+print("Hello Python! This is two line string.\nThis is second line")
+
+# concatenate string
+print("String 1" + "String 2")  # Concatenating with + no extra space is added in between the strings
+print("String 1","String 2")    # Concatenating with , an extra space is added in between the strings
+
+# Program ends here
