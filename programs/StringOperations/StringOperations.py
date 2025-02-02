fa605902affdaf3c6ca2e6477a855eb4bcc19c56
bishal <bishalgoutam3@gmail.com>
2025-02-01 19:55:15 -0500

    Updated String Operations

--- programs/StringOperations/StringOperations.py
+++ programs/StringOperations/StringOperations.py
@@ -9,8 +9,78 @@ print('A')
 print("Hello Python! This is one line string.")
 print("Hello Python! This is two line string.\nThis is second line")
 
-# concatenate string
+# concatenate string using + and comma
 print("String 1" + "String 2")  # Concatenating with + no extra space is added in between the strings
 print("String 1","String 2")    # Concatenating with , an extra space is added in between the strings
 
+# string replication using integer/count multiplication
+print("String One " * 3)
+# pattern 1
+print("*" * 1)
+print("*" * 2)
+print("*" * 3)
+print("*" * 4)
+print("*" * 5)
+# pattern 2
+print(" "*5 + "*"*1)
+print(" "*4 + "*"*2)
+print(" "*3 + "*"*3)
+print(" "*2 + "*"*4)
+print(" "*1 + "*"*5)
+
+# Check string length using len() function
+print(len("Word"))   # =4
+print(len("This is a sentence."))  # =19
+
+# String indexes
+name = "clear"
+ln = len(name)
+print(name[0])  #c
+print(name[1])  #l
+print(name[2])  #e
+print(name[3])  #a
+print(name[4])  #r
+print("\n")
+print(name[-1]) #r
+print(name[-2]) #a
+print(name[-3]) #e
+print(name[-4]) #l
+print(name[-5]) #c
+print("\n")
+print(name[-5]) #c
+print(name[-4]) #l
+print(name[-3]) #e
+print(name[-2]) #a
+print(name[-1]) #r
+print("\n")
+print(name[-ln]) #c
+print(name[-ln+1]) #l
+print(name[-ln+2]) #e
+print(name[-ln+3]) #a
+print(name[-ln+4]) #r
+
+# String slicing
+print(name[0:5]) # 0 to 5-1
+print(name[1:5]) # 1 to 5-1
+print(name[2:5]) # 2 to 5-1
+print(name[3:5]) # 3 to 5-1
+print(name[4:5]) # 4 to 5-1
+print(name[0:5]) # 0 to 5-1
+print(name[0:4]) # 0 to 4-1
+print(name[0:3]) # 0 to 3-1
+print(name[0:2]) # 0 to 2-1
+print(name[0:1]) # 0 to 1-1
+print(name[0:0]) # 0 to 0-1  # Nothing gets printed. No error
+
+print(name[0:]) # 0 to last
+print(name[1:]) # 1 to last
+print(name[2:]) # 2 to last
+print(name[3:]) # 3 to last
+print(name[4:]) # 4 to last
+print(name[:5]) # 0 to 5-1
+print(name[:4]) # 0 to 4-1
+print(name[:3]) # 0 to 3-1
+print(name[:2]) # 0 to 2-1
+print(name[:1]) # 0 to 1-1
+print(name[:0]) # 0 to 0-1  # Nothing gets printed. No error
 # Program ends here
